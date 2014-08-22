2ae14103c64e25e9444127c57443edd29588fe17
gchadwick <g@unknown14109fd4b261.att.net>
2014-08-21 17:47:37 -0700

    Prints max CxxCHs in single protein
    Prints all results to terminal, saving file is commented out

--- CXXCH_finder.py
+++ CXXCH_finder.py
@@ -8,12 +8,21 @@ path = input_file[0:string.rfind(input_file,'/')+1]
 os.chdir(path)
 records = SeqIO.parse(open(input_file,'rU'), 'fasta')
 p = re.compile('c..ch',re.IGNORECASE)
+counter = 0
+counter2 = 0
+largest = 0
 for record in records:
+	counter2+=1
 	sequence = str(record.seq)
 	if len(p.findall(sequence))>0:
 		print record.name
 		print record.seq
-		print p.findall(sequence)
+		print 'number:',len(p.findall(sequence)), p.findall(sequence)
+		if len(p.findall(sequence))>largest:
+			largest = len(p.findall(sequence))
+		counter+=1
+print counter, 'of', counter2
+print 'most CXXCHs:',largest
 #out_file = path + 'CXXCH_' + input_file[string.rfind(input_file,'/')+1:len(input_file)]
 #output_handle = open(out_file,'w')
 #SeqIO.write(sub_genomes,output_handle,'fasta')
